95a4332a49a8260d1245a4d8c61ee6468437dd09
liukun <liukundavid@gmail.com>
2024-05-18 13:47:45 +0800

    fix: telegram, twitter icon and layouts

--- components/NavigationHeader.tsx
+++ components/NavigationHeader.tsx
@@ -9,56 +9,56 @@ const NavigationHeader = () => {
   };
 
   return (
-    <div className='w-full p-10'>
-      <div className='border-[3px] border-black rounded-full h-[85px] bg-white'>
-        <div className='border-[3px] border-black rounded-full h-full bg-white relative right-[5px] bottom-[3px] flex justify-between items-center font-chakraPetch font-bold text-[24px] leading-[31.2px]'>
-          <div className='flex justify-start items-center gap-10 ml-16'>
-            <div className='flex justify-center items-center'>
-              <img src='/bird5.svg' alt='sack bird' width={55} height={55} />
+    <div className="w-full p-10">
+      <div className="border-[3px] border-black rounded-full h-[85px] bg-white">
+        <div className="border-[3px] border-black rounded-full h-full bg-white relative right-[5px] bottom-[3px] flex justify-between items-center font-chakraPetch font-bold text-[24px] leading-[31.2px]">
+          <div className="flex justify-start items-center gap-10 ml-16">
+            <div className="flex justify-center items-center">
+              <img src="/bird5.svg" alt="sack bird" width={55} height={55} />
             </div>
             <div
-              className='cursor-pointer'
+              className="cursor-pointer"
               onClick={() => handleScrollToSection("home")}
             >
               Home
             </div>
             <div
-              className='cursor-pointer'
+              className="cursor-pointer"
               onClick={() => handleScrollToSection("story")}
             >
               STORY
             </div>
             <div
-              className='cursor-pointer'
+              className="cursor-pointer"
               onClick={() => handleScrollToSection("sacknomics")}
             >
               SACKNOMICS
             </div>
             <div
-              className='cursor-pointer'
+              className="cursor-pointer"
               onClick={() => handleScrollToSection("roadmap")}
             >
               ROADMAP
             </div>
           </div>
-          <div className='flex justify-start items-center gap-16 flex-row-reverse mr-16'>
+          <div className="flex justify-start items-center flex-row-reverse mr-10">
             <div
-              className='w-9 h-9 bg-[#D9D9D9] rounded-full cursor-pointer'
+              className="cursor-pointer"
               onClick={() =>
                 (window.location.href = "https://twitter.com/BurnTheBird2001")
               }
             >
-              <img src='/telegram.png' alt='telegram' width={36} height={36} />
+              <img src="/telegram.svg" alt="telegram" width={35} height={35} />
             </div>
             <div
-              className='w-9 h-9 bg-[#D9D9D9] rounded-full cursor-pointer'
+              className="cursor-pointer mr-[26px]"
               onClick={() =>
                 (window.location.href = "https://t.me/burnthebird")
               }
             >
-              <img src='/twitter.svg' alt='telegram' width={36} height={36} />
+              <img src="/twitter.svg" alt="telegram" width={35} height={35} />
             </div>
-            <div className='cursor-pointer'>Buy Sack</div>
+            <div className="cursor-pointer mr-10">Buy Sack</div>
           </div>
         </div>
       </div>
